9399a807ed425102ef057a2750967f602598d5c5
Ilya Buziuk <ilyabuziuk@gmail.com>
2016-05-29 15:14:18 +0200

    Changing port to 5000

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 app.use('/', express.static(__dirname));
 app.listen(port, function() { console.log('listening: ' + port); });
